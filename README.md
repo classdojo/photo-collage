482f9879cb16f50e19395ee129732679dbf58457
Peter Hayes <peter.k.hayes@gmail.com>
2016-05-12 15:23:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# photo-mosaic
+Creates a canvas 
+
+## Installation
+`npm install --save photo-mosaic`.
+This library depends on `node-canvas`, which may require additional setup. See [their installation page](https://github.com/Automattic/node-canvas/wiki/_pages) for details.
+
+## Usage
+The following example creates a 2x3 mosaic from a variety of image sources.
+```js
+const createMosaic = require("photo-mosaic");
+
+const options = {
+  sources: [
+    imageBufferObject, // source can be a buffer of jpg/png data
+    canvasObject, // source can be a canvas object
+    "http://myurl.com/image.jpg", // strings that look like urls are downloaded
+    "ftp://myftpserver.com/image.png",
+    "https://mysecureurl.com/image.jpg"
+    "./localfile.png", // other strings are loaded as files
+  ],
+  width: 3,
+  height: 2,
+  imageWidth: 500,
+  imageHeight: 500,
+  backgroundColor: "#cccccc", // optional, defaults to black.
+  spacing: 2, // optional: pixels between each image
+};
+
+createMosaic(options)
+  .then((canvas) => {
+    canvas.jpegStream().pipe(fs.createWriteStream("myFile"))
+  });
+```
---
+++ index.js
@@ -0,0 +1,73 @@
+"use strict";
+
+const Promise  = require("bluebird");
+const request  = require("request-promise");
+const Canvas   = require("canvas");
+const fs       = Promise.promisifyAll(require("fs"));
+
+function getPhoto (src) {
+  if (src instanceof Buffer) {
+    return src;
+  } else if (typeof src === "string") {
+    if (/^http/.test(src) || /^ftp/.test(src)) {
+      return request(src)
+        .catch(() => {throw new Error(`Could not download url source: ${src}`);});
+    } else {
+      return fs.readFileAsync(src)
+        .catch(() => {throw new Error(`Could not load file source: ${src}`);});
+    }
+  } else if (src instanceof Canvas) {
+    return src.toBuffer();
+  } else {
+    throw new Error(`Unsupported source type: ${src}`);
+  }
+}
+
+const PARAMS = [
+  {field: "sources", required: true},
+  {field: "width", required: true},
+  {field: "height", required: true},
+  {field: "imageWidth", required: true},
+  {field: "imageHeight", required: true},
+  {field: "spacing", default: 0},
+  {field: "backgroundColor", default: "#000000"},
+];
+
+module.exports = function (options) {
+  if (Array.isArray(options)) {
+    options = {sources: options};
+  }
+
+  PARAMS.forEach((param) => {
+    if (options[param.field]) {
+      return;      
+    } else if (param.default) {
+      options[param.field] = param.default;
+    } else if (param.required) {
+      throw new Error(`Missing required option: ${param.field}`);
+    }
+  });
+
+  const canvasWidth = options.width * options.imageWidth + (options.width - 1) * (options.spacing);
+  const canvasHeight = options.height * options.imageHeight + (options.height - 1) * (options.spacing);
+  const canvas = new Canvas(canvasWidth, canvasHeight);
+
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = options.backgroundColor;
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+  return Promise
+    .map(options.sources, getPhoto)
+    .each((photoBuffer, i) => {
+      if (i >= options.width * options.height) return;
+
+      const x = (i % options.width) * (options.imageWidth + options.spacing);
+      const y = Math.floor(i / options.width) * (options.imageHeight + options.spacing);
+
+      const img = new Canvas.Image();
+      img.src = photoBuffer;
+
+      ctx.drawImage(img, x, y, options.imageWidth, options.imageHeight);
+    })
+    .return(canvas);
+};
